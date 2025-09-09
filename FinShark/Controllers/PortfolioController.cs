1d11ee1ff52c2f8bb4416ff9ab5fe18859f817e4
Teddy-GH <teddygirma20@gmail.com>
2025-09-09 21:54:37 +0300

    Portfolio cotroller intial commit

---
+++ FinShark/Controllers/PortfolioController.cs
@@ -0,0 +1,21 @@
+﻿using FinShark.Interfaces;
+using FinShark.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinShark.Controllers
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepo;
+        public PortfolioController(UserManager<AppUser> userManager, IStockRepository stockRepo)
+        {
+            _userManager = userManager;
+            _stockRepo = stockRepo;
+        }
+    }
+}
